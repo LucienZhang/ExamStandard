53c5c4ad4870796a28c3a1660d2f2878f0586b8d
Kevin <kevin.huo@basebit.ai>
2019-09-12 13:16:26 +0800

    tmp

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import json
+import sys
+import getopt
 
 
+# 读取json数据
 def load_file(file_name="goldset.json"):
     result = []
     line_count = 0
@@ -19,6 +22,7 @@ def load_file(file_name="goldset.json"):
                 print(e)
                 print('error line: {}'.format(line))
     print('Read source file finished: total={}, valid={}\n'.format(line_count, count))
+
     return result
 
 
@@ -27,7 +31,29 @@ def split_target(origin_target):
     idx = [0]
     for i in range(len(origin_target)):
         if origin_target[i][2] == "vector_seg":
+
+            # 特殊情况, 当遇到 "左手腕" + "正位片" + vector_seg 时，这种seg不分割
+            # [6, 6, 'symptom_pos', '左'],
+            # [7, 7, 'symptom_obj', '手'],
+            # [9, 9, 'symptom_obj', '腕'],
+            # [11, 13, 'exam', '正位片'],
+            # [24, 24, 'vector_seg', '，'],
+            # [25, 26, 'symptom_obj', '尺骨'],
+            # [27, 28, 'object_part', '茎突'],
+            # [29, 30, 'symptom_deco', '开始'],
+            # [31, 32, 'symptom_desc', '形成'],
+            # [33, 33, 'vector_seg', '，'],
+            # [40, 41, 'exam', '骨龄'],
+            # [42, 47, 'exam_result', '约为6.5岁'],
+            # [48, 48, 'vector_seg', '。'],
+            if i >= 2:
+                if origin_target[i-1][2] == "exam":
+                    if origin_target[i-2][2] == "symptom_obj":
+                        continue
+
+            # 其他情况正常分割
             idx.append(i)
+
     res = []
     for j in range(len(idx) - 1):
         res.append(origin_target[idx[j]:idx[j+1]])
@@ -36,6 +62,33 @@ def split_target(origin_target):
             if m[2] == "vector_seg":
                 k.pop(k.index(m))
     return res
+
+
+def add_optional_parameters(data):
+    # 函数所需的参数: data 是加载的初始数据
+    # -h --help
+    # --text= 输出第x个初始文本
+    # --target= 输出第x个标签
+    opts, args = getopt.getopt(sys.argv[1:], "-h-o:", ["help", "json=", "csv=",
+                                                       "text=", "target=", "output="])
+
+    for opt_name, opt_value in opts:
+        if opt_name in ("-h", "--help"):
+            print(r'''Optional parameters:
+            python load_data_script.py --text <n>  // Print n-th text
+            python load_data_script.py --target <n>  // Print n-th target
+            ''')
+            exit()
+
+        elif opt_name in ("--text",):
+            n = int(opt_value)
+            print("\n第%d个文本\n" % n)
+            print(data[n]["input"]["text"])
+        elif opt_name in ("--target",):
+            n = int(opt_value)
+            print("\n第%d个标签\n" % n)
+            for tag in data[n]["target"]:
+                print(str(tag) + ",")
 
 
 def print_segment(segment):
---
+++ v4.py
@@ -0,0 +1,119 @@
+from itertools import product
+
+
+# 之前将所有标签放入一个统一 stack
+# v4.py 分成多个 stack (ppo_stack, ir_stack, exam_stack等)
+
+
+def _build_product_param(*test_stacks):
+    ans = []
+    for test_stack in test_stacks:
+        if len(test_stack) > 0:
+            ans.append(test_stack)
+    return ans
+
+
+x = [
+    [0, 1, 'symptom_obj', '胸廓'],
+    [2, 3, 'symptom_pos', '两侧'],
+    [2, 3, 'exam_item', '形态'],
+    [4, 5, 'exam_result', '正常'],
+    [6, 7, 'exam_result', '清楚']
+]
+
+pos_or_obj_or_part = ["symptom_pos", "symptom_obj", "object_part"]
+
+ppos = []
+ppo_stack = []
+
+items = []
+ir = []
+
+exam_stack = []
+
+treatment_stack = []
+medical_events = []
+
+res_x = []
+
+for i in range(len(x)):
+    tag = x[i][2]
+    value = "$" + x[i][2] + "&" + x[i][3] + "***"
+
+    if tag == "symptom_pos":
+        ppos.append([value])
+
+    elif tag == "symptom_obj":
+        if len(ppos) == 0:
+            ppos.append(value)
+            if x[i + 1][2] not in pos_or_obj_or_part:
+                ppo_stack.append(value)
+        else:
+            if tag not in [j[j.index("$") + 1:j.index("&")] for j in ppos]:
+                ppos.append(x[i][2:])
+            else:
+                # 这里做两个obj之间的关系判断, 目前先直接拼一起
+                # 例子: ppo_stack = ["$obj&肾"]
+                # 加入新的后: []
+                pass
+
+    elif tag == "exam_item":
+        items.append(value)
+    elif tag == "exam_result":
+        # 和 item 拼接, 并放入ir (item_result简写)
+        if len(items) > 0:
+            ir.extend([j + value for j in items])
+        else:
+            ir.append(value)
+
+        # 情况1举例 (i == len(x) - 1)
+        # 遇到"正常"时，不会输出到 res_x中;
+        # 遇到"清楚"时, 会做2件事: 输出到res_x + 清空items, ir, ppo_stack
+        # [0, 1, 'symptom_obj', '头颅'],
+        # [2, 3, 'exam_item', '形态'],
+        # [4, 5, 'exam_result', '正常'],
+        # [4, 5, 'exam_result', '清楚']
+        if i == len(x) - 1:
+            # 将各个stack放入 itertools.product 函数所需的参数中
+            product_params = _build_product_param(ppo_stack, ir, exam_stack, treatment_stack)
+
+            # itertools.product
+            prod_res = list(product(*product_params))
+
+            # 将拼出的结构化数据, 写入当前seg的res_x结果列表中
+            res_x.extend(["".join(j) for j in prod_res])
+
+            # 清空 items, ir, ppo_stack
+            items, ir, ppo_stack = [], [], []
+
+        # 情况2举例 i < len(x) - 1)
+        # 当遇到"均匀", "不宽"时，都会做2件事: 输出 + 清空变量
+        # [9, 11, 'symptom_obj', '头部'],
+        # [17, 19, 'exam_item', '骨密度'],
+        # [20, 21, 'exam_result', '均匀'],
+        # [23, 24, 'exam_item', '颅缝'],
+        # [25, 26, 'exam_result', '不宽']
+        # ...
+        if i < len(x) - 1:
+            if x[i + 1][2] != tag:
+                # 将各个stack放入 itertools.product 函数所需的参数中
+                product_params = _build_product_param(ppo_stack, ir, exam_stack, treatment_stack)
+
+                # itertools.product
+                prod_res = list(product(*product_params))
+
+                # 将拼出的结构化数据, 写入当前seg的res_x结果列表中
+                res_x.extend(["".join(j) for j in prod_res])
+
+                # 清空 items, ir, ppo_stack
+                items, ir, ppo_stack = [], [], []
+
+
+if __name__ == "__main__":
+    print('items: ', items)
+    print("ir: ", ir)
+    print("ppos: ", ppos)
+    print("ppo_stack: ", ppo_stack)
+    print("product_param: ", product_params)
+    print("prod_res: ", prod_res)
+    print("res_x: ", res_x)
