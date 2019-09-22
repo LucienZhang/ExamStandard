77c68a40e9864d7951dfee5350eb03bedc15a8dd
Kevin <kevin.huo@basebit.ai>
2019-09-22 18:58:06 +0800

    update lesion_desc.py

--- core/handle_funcs/lesion_desc.py
+++ core/handle_funcs/lesion_desc.py
@@ -5,11 +5,10 @@ from core.logic.bu_build_work_flow import build_work_flow
 def handle_lesion_desc(seg, res_seg, i, stack):
     stack["lesion_desc"] = [seg[i]]
 
-    if len(stack["lesion"]) > 0:
-        tmp_ll_stack = [stack["lesion"], [seg[i]]]
-        tmp_ll_stack.sort(key=get_sort_key)
-        stack["ll_stack"] = ["".join([connect_tag_and_value(tmp[0]) for tmp in tmp_ll_stack])]
+    tmp_ll_stack = [stack["lesion"], [seg[i]]]
+    tmp_ll_stack.sort(key=get_sort_key)
+    stack["ll_stack"] = ["".join([connect_tag_and_value(tmp[0]) for tmp in tmp_ll_stack])]
 
-        res_seg, stack = build_work_flow(seg, res_seg, i, stack)
+    res_seg, stack = build_work_flow(seg, res_seg, i, stack)
 
     return res_seg, stack
