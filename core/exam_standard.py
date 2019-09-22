47df33960590bed9a098acc03ef100432082f80f
Kevin <kevin.huo@basebit.ai>
2019-09-22 17:34:11 +0800

    重新定义self.res_seg/ res_segments/ res_all

--- core/exam_standard.py
+++ core/exam_standard.py
@@ -8,8 +8,9 @@ class ExamStandardProcessor(object):
     结构化拼接类.
     输入 source_json_file_path/name: 输入源json文件, 包含原检查报告和标注结果
     输出 all_result: 结构化拼接出的结果
-    self.all_result: 100个样本结果的总和, 即所有 output_list 之和
-    self.output_list: 1个样本中所有seg的结果总和, 即所有 res_seg 之和
+
+    self.res_all: 100个样本结果的总和, 即所有 res_segments 之和
+    self.res_segments: 1个样本中所有seg的结果总和, 即所有 res_seg 之和
     self.res_seg: 1个seg中所有结果之和
     self.data: load_source_json获得的初始数据, 需要传递到 process_seg_one函数中
     """
@@ -17,10 +18,10 @@ class ExamStandardProcessor(object):
     def __init__(self, file_path, file_name):
         self.file_path = file_path
         self.file_name = file_name
-        self.res_seg = []
         self.data = []
-        self.output_list = []
-        self.all_result = []
+        self.res_seg = []
+        self.res_segments = []
+        self.res_all = []
 
     def load_source_json_file(self):
         abs_file_path = self.file_path + self.file_name
@@ -45,7 +46,7 @@ class ExamStandardProcessor(object):
         stack = init_stack()
         self.res_seg = process_seg_one(seg, text, res_seg=self.res_seg, stack=stack)
 
-        self.output_list.append(self.res_seg)
+        self.res_segments.append(self.res_seg)
         self.res_seg = []
 
     def put_output_list_to_all_result(self, data_idx):
@@ -54,11 +55,11 @@ class ExamStandardProcessor(object):
         :param data_idx: 该参数的取值范围, 就是 source_json_file 中的样本数量(100左右)
         """
 
-        self.all_result.append(
-            {data_idx: self.output_list}
+        self.res_all.append(
+            {data_idx: self.res_segments}
         )
 
-        self.output_list = []
+        self.res_segments = []
 
     def save_all_result_to_json(self, result_save_path, result_save_name):
-        Utils.save_all_result_to_json(self.data, self.all_result, result_save_path, result_save_name)
+        Utils.save_all_result_to_json(self.data, self.res_all, result_save_path, result_save_name)
