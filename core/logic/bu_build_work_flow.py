c9851cdc8aaeac4c25fb1f30c0b7e11e7b1b6f3b
Kevin <kevin.huo@basebit.ai>
2019-09-22 19:03:26 +0800

    更新bu_build_work_flow.py注释

--- core/logic/bu_build_work_flow.py
+++ core/logic/bu_build_work_flow.py
@@ -6,6 +6,15 @@ from core.utils import check_build_timing
 
 
 def build_work_flow(seg, res_seg, i, stack):
+    """
+    遇到 exam_result, reversed_exam_item, lesion_desc, symptom_desc, treatment_desc 时调用
+    :param seg: 子seg
+    :param res_seg: 存储该seg的结果
+    :param i: 当前标签在seg中的索引
+    :param stack: stack = {"ppos": [], "ppo_stack": [], "lesion_stack": [], ...}
+    :return: itertools.product 拼接的结果
+    """
+    
     # step 1 将ppos中的项, 按照不同情况拼接后，放入ppo_stack中
     stack["ppo_stack"] = build_ppo_stack(ppos=stack["ppos"], ppo_stack=stack["ppo_stack"])
 
@@ -14,7 +23,6 @@ def build_work_flow(seg, res_seg, i, stack):
 
     if timing:
         # 构造 product_param
-        stack["treatment_desc"] = [seg[i]]
         args, kwargs = get_args_kwargs_for_build_prod_param_func(seg[i][2], stack)
         product_params = build_sorted_product_params(*args, **kwargs)
 
@@ -23,10 +31,6 @@ def build_work_flow(seg, res_seg, i, stack):
 
         # 结果存入 res_x
         res_seg.extend(["".join(j) for j in prod_res])
-
-        # 清空 tt_stack 和 treatment
-        stack["tt_stack"] = []
-        stack["treatment"] = []
 
     if clean_ppo_stack:
         stack["ppo_stack"] = []
