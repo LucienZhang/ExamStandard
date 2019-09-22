9827848ef4a7f661eeb31f44bd60a60e9436da37
Kevin <kevin.huo@basebit.ai>
2019-09-22 17:24:05 +0800

    配置文件

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-# 作为输入的json文件路径
+# 输入的 json 源文件路径
 source_json_file_path = "/users/hk/dev/ExamStandard/data/"
 source_json_file_name = "goldset_93.json"
 
-# 存储结果的json文件路径
+# 存储结果的 json 文件路径
 result_save_path = "/users/hk/dev/ExamStandard/data/"
 result_save_name = "result_%s.json" % datetime.now().strftime('%y-%m-%d %I:%M:%S %p')
