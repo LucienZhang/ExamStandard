a2e338bdbd6bfd73c58722b2f272dc88992ef4ca
Kevin <kevin.huo@basebit.ai>
2019-09-23 14:14:27 +0800

    更新handle_exam_result和 reversed_exam_item.py

--- core/handle_funcs/exam_result.py
+++ core/handle_funcs/exam_result.py
@@ -4,7 +4,7 @@ from core.logic.bu_build_work_flow import build_work_flow
 
 def handle_exam_result(seg, res_seg, i, stack):
     # step 1
-    stack["exam_result_stack"] = [seg[i]]
+    stack["exam_result_stack"].append(connect(seg[i]))
 
     # step 3 build 结果
     res_seg, stack = build_work_flow(seg, res_seg, i, stack)
--- core/handle_funcs/reversed_exam_item.py
+++ core/handle_funcs/reversed_exam_item.py
@@ -1,14 +1,9 @@
-from core.utils import connect_tag_and_value
+from core.utils import connect
 from core.logic.bu_build_work_flow import build_work_flow
 
 
 def handle_reversed_exam_item(seg, res_seg, i, stack):
-    # step 1 把自己和 results 中的项拼接, 然后放入 reversed_ir 列表中 (不用考虑entity_neg)
-    if len(stack["results"]) > 0:
-        stack["reversed_ir"].extend([connect_tag_and_value(j) +
-                                     connect_tag_and_value(seg[i]) for j in stack["results"]])
-    else:
-        stack["reversed_ir"].append(connect_tag_and_value(seg[i]))
+    stack["reversed_exam_item_stack"].append(connect(seg[i]))
 
     res_seg, stack = build_work_flow(seg, res_seg, i, stack)
 
