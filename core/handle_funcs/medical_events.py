c042ee530e179e42b03a25f9384486a987aa7029
Kevin <kevin.huo@basebit.ai>
2019-09-23 14:11:56 +0800

    更新handle_medical_events.py

--- core/handle_funcs/medical_events.py
+++ core/handle_funcs/medical_events.py
@@ -1,8 +1,7 @@
-from core.utils import connect_tag_and_value
+from core.utils import connect
 
 
 def handle_medical_events(seg, res_seg, i, stack):
-    stack["medical_events"] = [seg[i]]
-    stack["medical_events_stack"] = [connect_tag_and_value(seg[i])]
+    stack["medical_events_stack"] = [connect(seg[i])]
 
     return res_seg, stack
