638959dc5b914cc30c4cb8897443b0c8865bb512
Kevin <kevin.huo@basebit.ai>
2019-09-23 14:23:14 +0800

    treatment_desc.py

--- core/handle_funcs/treatment_desc.py
+++ core/handle_funcs/treatment_desc.py
@@ -1,20 +1,15 @@
-from core.utils import connect_tag_and_value
+from core.utils import connect
 from core.logic.bu_build_work_flow import build_work_flow
 
 
 def handle_treatment_desc(seg, res_seg, i, stack):
     # step 1
-    stack["treatment_desc"] = [seg[i]]
+    stack["treatment_desc_stack"] = [connect([seg[i]])]
 
-    # step 2 与 treatment 拼接
-    if len(stack["treatment"]) > 0:
-        stack["tt_stack"] = [connect_tag_and_value(stack["treatment"][0]) +
-                             connect_tag_and_value(seg[i])]
-
-    # step 3 build
+    # step 2 build
     res_seg, stack = build_work_flow(seg, res_seg, i, stack)
 
-    stack["tt_stack"] = []
-    stack["treatment"] = []
+    # 清空变量
+    stack["treatment_stack"] = []
 
     return res_seg, stack
