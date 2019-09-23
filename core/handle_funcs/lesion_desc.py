9d9e1233ad8e069c3f71523541e9d78819b8bd97
Kevin <kevin.huo@basebit.ai>
2019-09-23 14:11:09 +0800

    修改handle_lesion_desc.py

--- core/handle_funcs/lesion_desc.py
+++ core/handle_funcs/lesion_desc.py
@@ -1,13 +1,9 @@
-from core.utils import get_sort_key, connect
+from core.utils import connect
 from core.logic.bu_build_work_flow import build_work_flow
 
 
 def handle_lesion_desc(seg, res_seg, i, stack):
     stack["lesion_desc_stack"] = [connect(seg[i])]
-
-    tmp_ll_stack = [stack["lesion_stack"], [seg[i]]]
-    tmp_ll_stack.sort(key=get_sort_key)
-    stack["ll_stack"] = ["".join([connect(tmp[0]) for tmp in tmp_ll_stack])]
 
     res_seg, stack = build_work_flow(seg, res_seg, i, stack)
 
