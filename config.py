3c403c6186937fc3aa2ff00f9d51594d9f8d88ff
Kevin <kevin.huo@basebit.ai>
2019-09-23 15:44:12 +0800

    清理无用注释

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 # 输入的 json 源文件路径
 source_json_file_path = "/users/hk/dev/ExamStandard/data/"
-source_json_file_name = "test.json"
+source_json_file_name = "goldset_93.json"
 
 # 存储结果的 json 文件路径
 result_save_path = "/users/hk/dev/ExamStandard/data/"
-result_save_name = "result_%s.json" % datetime.now().strftime('%y-%m-%d %I:%M:%S %p')
+result_save_name = "result_%s.json" % datetime.now().strftime('%y-%m-%d_%I:%M:%S__%p')
--- core/init_stack.py
+++ core/init_stack.py
@@ -1,16 +1,3 @@
-# stack_key_list = [
-#     "ppos", "ppo_stack",
-#     "items", "ir",
-#     "decorations", "deco_desc",
-#     "results", "reversed_ir",
-#     "entity_neg", "entity_neg_stack",
-#     "lesion", "lesion_stack", "ll_stack",
-#     "exam", "exam_stack",
-#     "time", "time_stack",
-#     "medical_events", "medical_events_stack",
-#     "treatment", "treatment_stack", "tt_stack"
-# ]
-
 stack_key_list = [
     "ppos", "ppo_stack",
     "exam_item_stack", "exam_result_stack",
