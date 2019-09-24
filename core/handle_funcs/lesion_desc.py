dbcdd34babe1240e60325d9bd7e83045fd6714ab
Kevin <kevin.huo@basebit.ai>
2019-09-24 12:38:08 +0800

    update build_work_flow

--- core/handle_funcs/lesion_desc.py
+++ core/handle_funcs/lesion_desc.py
@@ -4,8 +4,4 @@ from core.logic.bu_build_work_flow import build_work_flow
 def handle_lesion_desc(seg, text, res_seg, i, stack):
     res_seg, stack = build_work_flow(seg, text, res_seg, i, stack)
 
-    if i < len(seg) - 1:
-        if seg[i+1][2] != seg[i][2]:
-            stack["symptom_deco"] = []
-
     return res_seg, stack
--- core/logic/bu_build_work_flow.py
+++ core/logic/bu_build_work_flow.py
@@ -7,33 +7,13 @@ from core.logic.bu_build_sorted_product_params import build_sorted_product_param
 
 def build_work_flow(seg, text, res_seg, i, stack):
     """
-    遇到 exam_result, reversed_exam_item, lesion_desc, symptom_desc, treatment_desc 时调用
+    遇到 exam_result, reversed_exam_item, lesion_desc, treatment_desc 时调用
     :param seg: 子seg
     :param res_seg: 存储该seg的结果
     :param i: 当前标签在seg中的索引
     :param stack: stack = {"ppos": [], "ppo_stack": [], "lesion_stack": [], ...}
     :return: itertools.product 拼接的结果
     """
-
-    # # 入栈
-    # stack["%s_stack" % seg[i][2]] = [connect(seg[i])]
-    #
-    # # 拼接 ppo_stack
-    # stack["ppo_stack"] = build_ppo_stack(ppos=stack["ppos"])
-    #
-    # # 构造 product_param
-    # args = get_product_params_func_args(seg[i][2], stack)
-    # product_params = build_sorted_product_params(*args)
-    #
-    # # 构造结构化结果
-    # prod_res = list(product(*product_params))
-    #
-    # # 结果存入 res_x
-    # for prod_res_One in prod_res:
-    #     res_seg.append(prod_res_One)
-    #
-    # # 清空 ppo_stack
-    # stack["ppo_stack"] = []
 
     stack[seg[i][2]].append(connect(seg[i]))
 
