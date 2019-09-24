9b979e8af3d55f9fb96cbe9111c02f70d65afac8
Kevin <kevin.huo@basebit.ai>
2019-09-24 16:26:20 +0800

    handle_lesion 增加清理lesion_desc stack

--- core/handle_funcs/lesion.py
+++ core/handle_funcs/lesion.py
@@ -4,4 +4,6 @@ from core.logic.bu_build_work_flow import build_work_flow
 def handle_lesion(seg, text, res_seg, i, stack):
     res_seg, stack = build_work_flow(seg, text, res_seg, i, stack)
 
+    stack["lesion_desc"] = []
+
     return res_seg, stack
