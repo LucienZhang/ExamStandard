9fa056c557862ad56fa48ef76af0893cc40eb249
Kevin <kevin.huo@basebit.ai>
2019-09-24 11:25:52 +0800

    增加text参数

--- core/handle_funcs/exam_result.py
+++ core/handle_funcs/exam_result.py
@@ -2,7 +2,7 @@ from core.logic.bu_build_work_flow import build_work_flow
 
 
 def handle_exam_result(seg, text, res_seg, i, stack):
-    res_seg, stack = build_work_flow(seg, res_seg, i, stack)
+    res_seg, stack = build_work_flow(seg, text, res_seg, i, stack)
 
     if i < len(seg) - 1:
         if seg[i+1][2] != seg[i][2]:
--- core/handle_funcs/reversed_exam_item.py
+++ core/handle_funcs/reversed_exam_item.py
@@ -2,7 +2,7 @@ from core.logic.bu_build_work_flow import build_work_flow
 
 
 def handle_reversed_exam_item(seg, text, res_seg, i, stack):
-    res_seg, stack = build_work_flow(seg, res_seg, i, stack)
+    res_seg, stack = build_work_flow(seg, text, res_seg, i, stack)
 
     # 清变量
     if i < len(seg) - 1:
