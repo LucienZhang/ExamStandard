5ceaaed9be84431391cb5762354ff9bafc5c3bba
Kevin <kevin.huo@basebit.ai>
2019-09-25 12:26:22 +0800

    更新symptom_deco入栈规则

--- core/handle_funcs/symptom_deco.py
+++ core/handle_funcs/symptom_deco.py
@@ -2,17 +2,6 @@ from core.utils import connect
 
 
 def handle_symptom_deco(seg, text, res_seg, i, stack):
-    deco_special_sit = 0
-
-    if "symptom_desc" not in [k[2] for k in seg[i:]]:
-        deco_special_sit = 1
-    else:
-        if i < len(seg) - 1:
-            if seg[i + 1][2] not in ["symptom_pos", "symptom_obj", "object_part"]:
-                if seg[i + 1][2] not in ["symptom_deco", "symptom_desc"]:
-                    deco_special_sit = 2
-
-    if deco_special_sit not in [1, 2]:
-        stack[seg[i][2]].append(connect(seg[i]))
+    stack[seg[i][2]].append(connect(seg[i]))
 
     return res_seg, stack
