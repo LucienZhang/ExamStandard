1964a04b911a49573fbcee95b365b9847e2f89c2
Kevin <kevin.huo@basebit.ai>
2019-09-25 12:17:02 +0800

    更新清空stack的2行代码

--- core/handle_funcs/reversed_exam_item.py
+++ core/handle_funcs/reversed_exam_item.py
@@ -4,9 +4,6 @@ from core.logic.bu_build_work_flow import build_work_flow
 def handle_reversed_exam_item(seg, text, res_seg, i, stack):
     res_seg, stack = build_work_flow(seg, text, res_seg, i, stack)
 
-    # 清 reversed_exam_result
-    if i < len(seg) - 1:
-        if seg[i+1][2] != seg[i][2]:
-            stack["reversed_exam_result"] = []
-
+    stack["reversed_exam_result"] = []
+    stack["reversed_exam_item"] = []
     return res_seg, stack
