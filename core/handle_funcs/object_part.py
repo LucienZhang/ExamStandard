866eb98196a8e24a1db1cdd83a9040203c6514bb
Kevin <kevin.huo@basebit.ai>
2019-09-26 16:25:20 +0800

    拆分出2个功能函数

--- core/handle_funcs/object_part.py
+++ core/handle_funcs/object_part.py
@@ -1,3 +1,73 @@
+# 功能函数 1
+def slice_sub_seg_between_current_object_part_and_next_comma(seg, text, i,):
+    """
+    该函数用来从seg中，截取一段sub_seg，这段sub_seg开始是当前seg[i], 结束是从text找到离当前seg[i]最近的一个标点符号
+    作用: 看一下如果这段sub_seg中有 symptom_obj, 那么说明腔内要和这个obj绑定，所以stack["ppos"]中的obj就要出栈
+
+    示例:
+    原文text = "膀胱壁无异常增厚，腔内未见异常密度，大小正常。"
+    当前seg[i] 是 [9, 10, "object_part", "腔内"]
+
+    :return: sub_seg = [[object_part, 腔内], [reversed_result, 未见异常], [reversed_item, 密度]]
+    """
+
+    # current_text_idx = 10, text_stop_idx = 10
+    # seg_stop_idx = 22
+    current_text_idx = seg[i][1]
+    text_stop_idx = seg[i][1]
+    seg_stop_idx = len(seg) - 1
+
+    for text_idx in range(current_text_idx, len(text)):
+        # 遇到了"异常密度"后面的逗号
+        # text_stop_idx = 17
+        if text[text_idx] in ["，", "。", "；"]:
+            text_stop_idx = text_idx
+            break
+    for tag_one_idx in range(i, len(seg)):
+        # [18, 19, "exam_item", "大小"] 中 18 > 17
+        # seg_stop_idx = 18
+        if seg[tag_one_idx][0] > text_stop_idx:
+            seg_stop_idx = tag_one_idx
+            break
+
+    return seg[i+1:seg_stop_idx+1]
+
+
+# 功能函数 2
+def check_comma_existence_between_a_and_b(ppos, text, current_pos_index_from_ppos):
+    """
+    :param ppos: 即 stack["ppos"]
+    :param current_pos_index_from_ppos: 当前在stack["ppos"] 中遇到的symptom_pos项, 在 stack["ppos"] 中的索引
+    a 和 b 是 stack["ppos"] 中的 2 项
+    该函数用来判断: a 和 b 之间在原文中是否有逗号
+
+    示例数据:
+    当前seg[i] == [37, 40, "object_part", "囊形成分"]
+    stack["ppos"] = [[0, 0, 'symptom_pos', '左'], [1, 1, 'symptom_obj', '肾'], [2, 4, 'symptom_pos', '周外侧']]
+    text = "左肾周外侧可见多房囊性病变，边界尚清，最大截面约3.7x1.7cm，增强后囊性成分未见明显强化。"
+
+    该函数会查看"肾"和"周外侧"之间是否有逗号，句号或者分号.
+    从结果中看出: "左肾周外侧" 是连续的，没有逗号，所以返回 False
+
+    :return: True, 说明有逗号; False 说明没有逗号
+    """
+
+    # stack["ppos"] = [[34,35,obj,左肾], [36,38,pos,周外侧]]
+    # 查看 "肾" 和 "周" 之间，是否有逗号，分号或者句号
+    has_comma_between_a_and_b = False
+
+    punctuations = ["，", "。", "；"]
+    j = current_pos_index_from_ppos
+
+    for comma in punctuations:
+        if comma in text[ppos[j - 1][1] + 1:ppos[j][0]]:
+            has_comma_between_a_and_b = True
+            break
+
+    return has_comma_between_a_and_b
+
+
+# 主函数
 def handle_obj_part(seg, text, res_seg, i, stack):
     """
     pop_up_count_from_ppos: 需要从 stack["ppos"] 中出栈的项的数量
@@ -45,49 +115,25 @@ def handle_obj_part(seg, text, res_seg, i, stack):
                         pop_up_count_from_ppos += 1
                         continue
 
-                    # 举例
+                    # 举例: 当前seg[i] = [9, 10, "object_part", "腔内"]
                     # 原文: "膀胱壁无异常增厚，腔内未见异常密度，大小正常。"
-                    # 假设当前seg[i] = [9, 10, "object_part", "腔内"]
-                    # current_text_idx = 10, text_stop_idx = 10
-                    # seg_stop_idx = 22
                     elif tag == "symptom_obj":
-                        current_text_idx = seg[i][1]
-                        text_stop_idx = seg[i][1]
-                        seg_stop_idx = len(seg) - 1
+                        # 调用功能函数 1
+                        sub_seg = slice_sub_seg_between_current_object_part_and_next_comma(seg, text, i)
 
-                        for text_idx in range(current_text_idx, len(text)):
-                            # 遇到了"异常密度"后面的逗号
-                            # text_stop_idx = 17
-                            if text[text_idx] in ["，", "。", "；"]:
-                                text_stop_idx = text_idx
-                                break
-                        for tag_one_idx in range(i, len(seg)):
-                            # [18, 19, "exam_item", "大小"] 中 18 > 17
-                            # seg_stop_idx = 18
-                            if seg[tag_one_idx][0] > text_stop_idx:
-                                seg_stop_idx = tag_one_idx
-                                break
-
-                        # seg[i+1:seg_stop_idx+1] = [revesed_result未见异常, reversed_item密度]
+                        # sub_seg = [revesed_result未见异常, reversed_item密度]
                         # 也就是说, 从"腔内"到下一个逗号这一段文字中，没有任何obj，也就是说：
-                        # 当前的part腔内无法和后面的obj绑定，只能和前面ppos中的obj绑定
-                        # 所以 ppos 中的 obj不用出栈，因为它还要继续和当前 part 绑定, 去拼接ppo_stack
-                        if "symptom_obj" not in [check_item[2] for check_item in seg[i+1:seg_stop_idx+1]]:
+                        # 当前的part腔内, 无法和后面的obj绑定(因为没有)，只能和前面ppos中的obj绑定
+                        # 所以 现在遇到的这个 obj"膀胱" 不用出栈，因为它还要继续和当前 part"腔内" 绑定.
+                        if "symptom_obj" not in [check_item[2] for check_item in sub_seg]:
                             break
 
                     elif tag == "symptom_pos":
                         if j > 0:
-                            # stack["ppos"] = [[34,35,obj,左肾], [36,38,pos,周外侧]]
-                            # 查看 "肾" 和 "周" 之间，是否有逗号，分号或者句号
-                            has_comma_between_A_and_B = False
-                            punctuations = ["，", "。", "；"]
+                            # 调用功能函数 2
+                            has_comma_between_a_and_b = check_comma_existence_between_a_and_b(stack["ppos"], text, j)
 
-                            for comma in punctuations:
-                                if comma in text[stack["ppos"][j - 1][1] + 1:stack["ppos"][j][0]]:
-                                    has_comma_between_A_and_B = True
-                                    break
-
-                            if has_comma_between_A_and_B:
+                            if has_comma_between_a_and_b:
                                 pop_up_count_from_ppos += 1
 
     # step 3 使用 pop_up_count_from_ppos， 将需要出栈的部分, 从 stack[ppos] 中切掉
