5994cd33d413a448b62c14d9d9e3a351f172a806
Kevin <kevin.huo@basebit.ai>
2019-09-26 09:34:54 +0800

    更新注释

--- core/logic/build_ppo_stack_funcs/build_ppo_stack_func_map.py
+++ core/logic/build_ppo_stack_funcs/build_ppo_stack_func_map.py
@@ -5,10 +5,17 @@ from core.logic.build_ppo_stack_funcs.pos_part_obj import build_ppo_stack_by_pos
 
 
 # {situation: func}
-# 1 obj
-# 2 obj part
-# 3 obj pos
-# 4 obj pos part
+# 1 stack["ppos"]中只有 obj, 例:
+# ppos = [["obj","肾"], ["obj","肝"], ["obj", "肺"]]
+
+# 2 stack["ppos"]中有 obj, part, 例:
+# ppos = [["obj","肾"], ["part","皮质"], ["obj", "肺"]]
+
+# 3 stack["ppos"]中有 obj pos, 例:
+# ppos = [["obj","肾"], ["pos","左侧"], ["obj", "肺"]]
+
+# 4 stack["ppos"]中有 obj pos part, 例:
+# ppos = [["obj","肾"], ["part","皮质"], ["pos","左侧"], ["obj", "肺"], ["part","肺泡"]]
 build_ppo_stack_func_map = {
     1: build_ppo_stack_by_obj,
     2: build_ppo_stack_by_part_obj,
