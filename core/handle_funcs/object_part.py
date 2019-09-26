b3e6eb10b9be2bcbc67b3aa9b9d7888d15e1dd03
Kevin <kevin.huo@basebit.ai>
2019-09-26 15:47:50 +0800

    更新handle_obj_part.py中遇到ppos[-1]="symptom_obj的处理代码"

--- core/handle_funcs/object_part.py
+++ core/handle_funcs/object_part.py
@@ -15,9 +15,9 @@ def handle_obj_part(seg, text, res_seg, i, stack):
                 2.2 若不是:
 
                     3 倒序查看stack["ppos"], 统计 pop_up_count_from_ppos 的值
-                        3.a 遇到part
-                        3.b 遇到obj
-                        3.c 遇到pos
+                        3.a 遇到part --> line 44-46
+                        3.b 遇到obj --> line 48-76
+                        3.c 遇到pos --> line 78-91
     """
 
     # step 1 定义初始变量 pop_up_count_from_ppos
@@ -45,8 +45,35 @@ def handle_obj_part(seg, text, res_seg, i, stack):
                         pop_up_count_from_ppos += 1
                         continue
 
+                    # 举例
+                    # 原文: "膀胱壁无异常增厚，腔内未见异常密度，大小正常。"
+                    # 假设当前seg[i] = [9, 10, "object_part", "腔内"]
+                    # current_text_idx = 10, text_stop_idx = 10
+                    # seg_stop_idx = 22
                     elif tag == "symptom_obj":
-                        break
+                        current_text_idx = seg[i][1]
+                        text_stop_idx = seg[i][1]
+                        seg_stop_idx = len(seg) - 1
+
+                        for text_idx in range(current_text_idx, len(text)):
+                            # 遇到了"异常密度"后面的逗号
+                            # text_stop_idx = 17
+                            if text[text_idx] in ["，", "。", "；"]:
+                                text_stop_idx = text_idx
+                                break
+                        for tag_one_idx in range(i, len(seg)):
+                            # [18, 19, "exam_item", "大小"] 中 18 > 17
+                            # seg_stop_idx = 18
+                            if seg[tag_one_idx][0] > text_stop_idx:
+                                seg_stop_idx = tag_one_idx
+                                break
+
+                        # seg[i+1:seg_stop_idx+1] = [revesed_result未见异常, reversed_item密度]
+                        # 也就是说, 从"腔内"到下一个逗号这一段文字中，没有任何obj，也就是说：
+                        # 当前的part腔内无法和后面的obj绑定，只能和前面ppos中的obj绑定
+                        # 所以 ppos 中的 obj不用出栈，因为它还要继续和当前 part 绑定, 去拼接ppo_stack
+                        if "symptom_obj" not in [check_item[2] for check_item in seg[i+1:seg_stop_idx+1]]:
+                            break
 
                     elif tag == "symptom_pos":
                         if j > 0:
